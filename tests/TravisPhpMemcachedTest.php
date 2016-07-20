3efdca977edcf2b663746e5731f6b2851a908387
178inaba <178inaba@users.noreply.github.com>
2016-07-20 12:44:08 +0900

    fix test

--- tests/TravisPhpMemcachedTest.php
+++ tests/TravisPhpMemcachedTest.php
@@ -4,35 +4,31 @@ use PHPUnit\Framework\TestCase;
 
 class TravisPhpMemcachedTest extends TestCase
 {
-    public function testTravisPhpMemcached()
+    public function testNoMemcache()
     {
+    }
+
+    public function testMemcache()
+    {
+        $existExt = extension_loaded('memcache');
+        $this->assertTrue($existExt);
+
         $m = null;
-        if (extension_loaded('memcached')) {
-            echo "use memcached.\n";
-            $m = new Memcached();
-        } elseif (extension_loaded('memcache')) {
+        if ($existExt) {
             echo "use memcache.\n";
             $m = new Memcache();
-        } else {
-            echo "memcache and memcached is not installed.\n";
         }
+    }
 
-        if ($m !== null) {
-            $m->addServer('localhost', 11211);
+    public function testMemcached()
+    {
+        $existExt = extension_loaded('memcached');
+        $this->assertTrue($existExt);
 
-            $m->set('int', 99);
-            $m->set('string', 'a simple string');
-            $m->set('array', [11, 12]);
-            /* expire 'object' key in 5 minutes */
-            $m->set('object', new stdclass, time() + 300);
-
-            echo("\n");
-            var_dump($m->get('int'));
-            var_dump($m->get('string'));
-            var_dump($m->get('array'));
-            var_dump($m->get('object'));
+        $m = null;
+        if ($existExt) {
+            echo "use memcached.\n";
+            $m = new Memcached();
         }
-
-        $this->assertEquals(1, 1);
     }
 }
